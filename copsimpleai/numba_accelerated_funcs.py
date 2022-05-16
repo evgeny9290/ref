5751c1cae9257511e348edfa8f9b3b8696b9c4f4
evgney tayarov <evgniy.t@gmail.com>
2022-05-16 11:55:27 +0300

    test

--- copsimpleai/numba_accelerated_funcs.py
+++ copsimpleai/numba_accelerated_funcs.py
@@ -60,6 +60,8 @@ def partial_eval_jitted_one(currSolVar: int, solutionVector: np.array, valuesM: 
     currVal = solutionVector[currSolVar]
     currM = valuesM[currVal]
 
+    # Test
+
     return currVal, currM
 
 
